4d1c4a4083590fb636d0f0e30c623c92dd36e0e4
Ryan Ackermann <ryan@ackermann.io>
2022-06-08 09:25:09 -0700

    Initial Commit

---
+++ ScrollHeader/ScrollHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollHeaderApp.swift
+//  ScrollHeader
+//
+//  Created by Ryan Ackermann on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrollHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
